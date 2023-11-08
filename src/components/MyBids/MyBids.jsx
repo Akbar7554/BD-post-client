e9fab7b33bd01d56f1ce67db240d23e7a476ac51
Akbar Hossain <hakbar431@gmail.com>
2023-11-08 20:27:44 +0600

    collecting data from internet or other resource to implement the website

--- src/components/MyBids/MyBids.jsx
+++ src/components/MyBids/MyBids.jsx
@@ -16,6 +16,7 @@ const MyBids = () => {
   }, [])
     console.log(user.email);
     
+    
     const jobInfo = useLoaderData()
     const { jobTitle, price, userEmail, buyerEmail, date } = jobInfo
     const [job, setJob] = useState(jobInfo)
