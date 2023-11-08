1133387649fba6a3279f5408e92b01cee5d5383e
Akbar Hossain <hakbar431@gmail.com>
2023-11-08 20:34:25 +0600

    if user added job it will redirect in my posted job where i show the user job details which he posted

--- src/components/MyBids/MyBids.jsx
+++ src/components/MyBids/MyBids.jsx
@@ -16,7 +16,6 @@ const MyBids = () => {
   }, [])
     console.log(user.email);
 
-    
     const jobInfo = useLoaderData()
     const { jobTitle, price, userEmail, buyerEmail, date } = jobInfo
     const [job, setJob] = useState(jobInfo)
